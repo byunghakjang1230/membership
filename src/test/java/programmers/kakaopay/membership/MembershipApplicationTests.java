57249de44f75f5b1eb0d273ddd6cf9920b637711
hilu0318 <hilu0318@gmail.com>
2021-06-30 14:33:49 +0900

    first commit

---
+++ src/test/java/programmers/kakaopay/membership/MembershipApplicationTests.java
@@ -0,0 +1,13 @@
+package programmers.kakaopay.membership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
